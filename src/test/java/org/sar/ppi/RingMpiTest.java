164a85cca4159e7acd86efd000cd86860795a809
n-peugnet <n.peugnet@free.fr>
2020-02-12 20:37:09 +0100

    run test from mpirun

--- src/test/java/org/sar/ppi/RingMpiTest.java
+++ src/test/java/org/sar/ppi/RingMpiTest.java
@@ -2,20 +2,34 @@ package org.sar.ppi;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class RingMpiTest
-{
+public class RingMpiTest {
     /**
-     * Rigorous Test :-)
+     * Quite dirty for now but at least it can run mpi.
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        RingMpi.main(new String[0]);
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws IOException {
+        String s = null;
+        Process p = Runtime.getRuntime().exec("./mpirun.sh 6 RingMpi");
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+        // read the output from the command
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+        // read any errors from the attempted command
+        while ((s = stdError.readLine()) != null) {
+            System.err.println(s);
+        }
+        assertTrue(true);
     }
 }
