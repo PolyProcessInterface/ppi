92795927b6967f5a87f3960c076fbd4ced18fe7b
n-peugnet <n.peugnet@free.fr>
2020-02-18 15:18:00 +0100

    add getCurrentNode and broadcast

--- src/main/java/org/sar/ppi/Infrastructure.java
+++ src/main/java/org/sar/ppi/Infrastructure.java
@@ -3,12 +3,20 @@ package org.sar.ppi;
 public abstract class Infrastructure {
 
     protected Protocol protocol;
+    protected Node currentNode;
 
-    
     public Infrastructure(Protocol protocol) {
         this.protocol = protocol;
     }
-    
+
+    /**
+     * @return the currentNode
+     */
+    public Node getCurrentNode() {
+        return currentNode;
+    }
+
     public abstract void run(String[] args);
     public abstract void send(Node dest, Object message);
+    public abstract void broadcast(Object messsage);
 }
--- src/main/java/org/sar/ppi/MpiInfrastructure.java
+++ src/main/java/org/sar/ppi/MpiInfrastructure.java
@@ -1,5 +1,6 @@
 package org.sar.ppi;
 
+import mpi.Comm;
 import mpi.MPI;
 import mpi.MPIException;
 import mpi.Status;
@@ -17,9 +18,11 @@ public class MpiInfrastructure extends Infrastructure {
     public void run(String[] args) throws PpiException {
         try {
             MPI.Init(args);
+            Comm comm = MPI.COMM_WORLD;
+            currentNode = new Node(comm.getRank());
             while (true) {
                 Object buf = new Object();
-                Status status = MPI.COMM_WORLD.recv(buf, 1, MPI.INT, MPI.ANY_SOURCE, MPI.ANY_TAG);
+                Status status = comm.recv(buf, 1, MPI.INT, MPI.ANY_SOURCE, MPI.ANY_TAG);
                 protocol.processMessage(new Node(status.getSource()), buf);
             }
         } catch (MPIException e) {
@@ -30,6 +33,11 @@ public class MpiInfrastructure extends Infrastructure {
     @Override
     public void send(Node dest, Object message) {
         // TODO Auto-generated method stub
-        
+    }
+
+    @Override
+    public void broadcast(Object messsage) {
+        // TODO Auto-generated method stub
+
     }
 }
