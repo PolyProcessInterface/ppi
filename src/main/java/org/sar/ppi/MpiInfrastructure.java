89c3b6bc6c14cc53d3893a901111dec07f384434
Atlaoui Tarik <atlaouitarik@.com>
2020-02-26 23:17:54 +0100

    Send object fonctionnel

--- src/main/java/org/sar/ppi/MpiInfrastructure.java
+++ src/main/java/org/sar/ppi/MpiInfrastructure.java
@@ -12,9 +12,15 @@ public class MpiInfrastructure extends Infrastructure {
 
 	protected boolean running = true;
 	protected Comm comm;
-
+	public final int MaxSize;
 	public MpiInfrastructure(NodeProcess process) {
 		super(process);
+		MaxSize = 1024;
+	}
+
+	public MpiInfrastructure(NodeProcess process,int max_message_lenth) {
+		super(process);
+		MaxSize = max_message_lenth;
 	}
 
 	@Override
@@ -26,11 +32,12 @@ public class MpiInfrastructure extends Infrastructure {
 			process.start();
 			while (running) {
 				Object buf;
-				byte [] tab = new byte[100];
-				Status status = comm.recv(tab, 100, MPI.BYTE, MPI.ANY_SOURCE, MPI.ANY_TAG);
-				System.out.println("tableau recus= ");
+				byte [] tab = new byte[MaxSize];
+				Status status = comm.recv(tab, MaxSize, MPI.BYTE, MPI.ANY_SOURCE, MPI.ANY_TAG);
+				//System.out.println("tableau recus= ");
 				printByteArray(tab);
 				buf =ContentHandler.RetriveObject(tab);
+			//	System.out.println("buff apres= "+buf);
 				process.processMessage(status.getSource(), buf);
 			}
 			MPI.Finalize();
@@ -43,8 +50,7 @@ public class MpiInfrastructure extends Infrastructure {
 	public void send(int dest, Object message)  throws PpiException{
 		try {
 			byte [] tab = ContentHandler.ParseObject(message);
-			System.out.print("tableau envoyer= ");
-			printByteArray(tab);
+			//System.out.println("Object envoyer= "+message);
 			comm.send(tab, tab.length, MPI.BYTE, dest, 1);
 		} catch (MPIException e) {
 			throw new PpiException("Send to" + dest + "failed", e);
