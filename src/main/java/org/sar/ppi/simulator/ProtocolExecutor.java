848cf0d6d635622c1deba4f1a4e81615d106ce37
Atlaoui Tarik <atlaouitarik@.com>
2020-04-10 19:25:56 +0200

    avancez

---
+++ src/main/java/org/sar/ppi/simulator/ProtocolExecutor.java
@@ -0,0 +1,18 @@
+package org.sar.ppi.simulator;
+
+import netscape.javascript.JSObject;
+import org.sar.ppi.Infrastructure;
+
+public class Simulator {
+    private Infrastructure infra;
+
+    public Simulator(Infrastructure infra_user){
+        infra=infra_user;
+    }
+
+    public void simulate(JSObject protocol){
+
+    }
+
+
+}
