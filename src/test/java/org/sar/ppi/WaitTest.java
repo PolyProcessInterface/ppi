711a643b967c3be1c8ae1e0f64606118c826683c
gnemmik <kimmeng.asn@gmail.com>
2020-05-26 09:36:56 +0200

    tests: run tests on github but skip MPI

--- src/test/java/org/sar/ppi/WaitTest.java
+++ src/test/java/org/sar/ppi/WaitTest.java
@@ -2,6 +2,7 @@ package org.sar.ppi;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.sar.ppi.mpi.MpiRunner;
 import org.sar.ppi.peersim.PeerSimRunner;
@@ -63,6 +64,7 @@ public class WaitTest extends NodeProcess {
 
 	@Test
 	public void MpiAnnotatedProcessTest() {
+		Assume.assumeTrue(Environment.mpirunExist());
 		String[] args = { WaitTest.class.getName(), MpiRunner.class.getName() };
 		Ppi.main(args);
 		assertTrue(true);
